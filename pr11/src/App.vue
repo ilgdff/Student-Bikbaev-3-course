79b377a90686593ff356ce84ad69c579a395017f
ilgdff <ddrugkillz@gmail.com>
2025-06-10 17:01:56 +0300

    Add files via upload

---
+++ pr11/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="counter">
+    <h1>{{ count }}</h1>
+    <button @click="count--">-1</button>
+    <button @click="count++">+1</button>
+    <button @mouseover="count = 0" class="reset">Reset</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+
+<style>
+.counter {
+  text-align: center;
+  margin-top: 100px;
+}
+button {
+  margin: 0 10px;
+  padding: 8px 16px;
+  font-size: 16px;
+}
+.reset:hover {
+  background-color: #ff0000;
+  color: white;
+}
+</style>
